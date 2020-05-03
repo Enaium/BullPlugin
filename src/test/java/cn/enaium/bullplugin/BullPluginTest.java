f75930197c85b0a62b86de383e034a571acc1e47
Enaium <32991121+Enaium@users.noreply.github.com>
2020-05-03 17:59:45 +0800

    Test

---
+++ src/test/java/cn/enaium/bullplugin/BullPluginTest.java
@@ -0,0 +1,9 @@
+package cn.enaium.bullplugin;
+
+/**
+ * Project: BullPlugin
+ * -----------------------------------------------------------
+ * Copyright © 2020 | Enaium | All rights reserved.
+ */
+public class BullPluginTest {
+}
